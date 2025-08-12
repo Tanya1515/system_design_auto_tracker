a5ceac2adce452570a84362612cd92d3d4479e2c
Tanya1515 <tanyaozerova1318@gmail.com>
2025-08-12 14:29:08 +0300

    add postgist extension to postgresql scheme

--- database/auto_tracker.sql
+++ database/auto_tracker.sql
@@ -1,50 +1,57 @@
+CREATE EXTENSION postgis;
+
+CREATE EXTENSION pgrouting;
+
 CREATE TABLE "Users" (
     "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    "login" VARCHAR(50) NOT NULL UNIQUE
-    "phone" VARCHAR(13) NOT NULL UNIQUE
+    "login" VARCHAR(30) NOT NULL UNIQUE
+    "phone" VARCHAR(20) NOT NULL UNIQUE
     "fcm_token" VARCHAR(255) UNIQUE NOT NULL,
-    "radius" SMALLINT CHECK (radius >= 0 AND radius <= 100)
+    "radius" INTEGER CHECK (radius >= 0 AND radius <= 100)
 );
 
 CREATE TABLE "UserToBrandType" (
     "user_id" UUID REFERENCES Users(id) ON DELETE CASCADE
     "brand_type_id" UUID REFERENCES BrandTypes(id) ON DELETE CASCADE
+    PRIMARY KEY (user_id, brand_type_id)
 );
 
 CREATE TABLE "UserToFuelType" (
     "user_id" UUID REFERENCES Users(id) ON DELETE CASCADE
     "fuel_type_id" UUID REFERENCES FuelTypes(id) ON DELETE CASCADE
+    PRIMARY KEY (user_id, fuel_type_id)
 );
 
 CREATE TABLE "Stations" (
     "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    "address" VARCHAR(50) NOT NULL UNIQUE
     "object_id" UUID REFERENCES Objects(id) 
     "brand_id" UUID REFERENCES BrandTypes(id)
     "rating" REAL NOT NULL
     "open_time" TIME NOT NULL,
     "close_time" TIME NOT NULL,
-    CHECK (close_time > open_time)
+    CHECK (rating >= 0 AND rating <= 5)
 );
 
 CREATE TABLE "ServiceToStation" (
     "service_id" UUID REFERENCES Services(id) ON DELETE CASCADE
     "station_id" UUID REFERENCES Stations(id) ON DELETE CASCADE
+    PRIMARY KEY (service_id, station_id)
 );
 
 CREATE TABLE "FuelTypeToStation" (
     "station_id" UUID REFERENCES Stations(id) ON DELETE CASCADE
     "fuel_type_id" UUID REFERENCES FuelTypes(id) ON DELETE CASCADE
+    PRIMARY KEY (station_id, fuel_type_id)
 );
 
 CREATE TABLE "FuelTypes" (
     "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    "fuel" VARCHAR(10) NOT NULL UNIQUE
+    "fuel" VARCHAR(20) NOT NULL UNIQUE
 );
 
 CREATE TABLE "BrandTypes" (
     "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    "brand" VARCHAR(10) NOT NULL UNIQUE
+    "brand" VARCHAR(30) NOT NULL UNIQUE
     "giff_id" UUID 
 );
 
@@ -59,11 +66,10 @@ CREATE TABLE "Services" (
 );
 
 CREATE TABLE "Objects" (
-    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),  
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     "object_type_id" UUID REFERENCES ObjectTypes(id)
-    "name" VARCHAR(50) NOT NULL UNIQUE
-    "latitude" DOUBLE PRECISION
-    "longtitude" DOUBLE PRECISION
+    "address" VARCHAR(50) NOT NULL UNIQUE
+    "coordinates" GEOMETRY(GEOMETRY, 4326) NOT NULL,
 );
 
-COMMENT ON COLUMN "BrandTypes"."giff_id" IS 'Identificator for picture in notification';
+COMMENT ON COLUMN "BrandTypes"."giff_id" IS 'Identificator for picture in notification, that is stored in S3-storage';
